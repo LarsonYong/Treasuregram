352f84c6eca5d112e2b6f5d3a26b4a8a6d7b40bd
Larson <jianbo1889@gmail.com>
2017-05-31 23:07:40 -0700

    before 1.1

--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render
+from .models import Treasure
 
 
 # Create your views here.
 def index(request):
-    name = 'Gold Nugget'
-    value = 1000.00
-    context = {'treasure_name': name, 'treasure_val': value}
-
+    treasures = Treasure.objects.all()
     return render(request, 'index.html', {'treasures': treasures})
 
 
-class Treasure:
-    def __init__(self, name, value, material, location):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
-
-treasures = [
-    Treasure('Gold Nugget', 500.00, 'gold', "Curly's Creek, NM"),
-    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Fails, CO"),
-    Treasure('Coffee Can', 20.00, 'tin', 'Acme, CA')
-]
+# class Treasure:
+#     def __init__(self, name, value, material, location, img_url):
+#         self.name = name
+#         self.value = value
+#         self.material = material
+#         self.location = location
+#         self.img_url = img_url
+#
+# treasures = [
+#     Treasure('Gold Nugget', 500.00, 'Gold', "Curly's Creek, NM", 'example.com/nugget.jpg'),
+#     Treasure("Fool's Gold", 0, 'Pyrite', "Fool's Fails, CO", 'cdn.shopify.com/s/files/1/0081/4402/products/sticker_grande.jpg?v=1479972797'),
+#     Treasure('Coffee Can', 20.00, 'Tin', 'Acme, CA', 'example.com/coffee-can.jpg')
+# ]
