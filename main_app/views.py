0569541761e1a57f11c43cbfc7cb882f99aac7ce
LarsonYong <jianbo1889@gmail.com>
2017-06-06 02:30:01 -0700

    add login/logout

--- main_app/views.py
+++ main_app/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from .forms import TreasureForm, LoginForm
 from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -19,39 +20,20 @@ def detail(request, treasure_id):
     treasure = Treasure.objects.get(id=treasure_id)
     return render(request, 'detail.html', {'treasure': treasure})
 
+
 def post_treasure(request):
     form = TreasureForm(request.POST, request.FILES)
     if form.is_valid():
-        # treasure = Treasure(name=form.cleaned_data['name'],
-        #                     value=form.cleaned_data['value'],
-        #                     material=form.cleaned_data['material'],
-        #                     location=form.cleaned_data['location'],
-        #                     img_url=form.cleaned_data['img_url'])
-        # treasure.save()
         treasure = form.save(commit=False)
         treasure.user=request.user
         treasure.save()
         return HttpResponseRedirect('/')
 
-
-# class Treasure:
-#     def __init__(self, name, value, material, location, img_url):
-#         self.name = name
-#         self.value = value
-#         self.material = material
-#         self.location = location
-#         self.img_url = img_url
-#
-# treasures = [
-#     Treasure('Gold Nugget', 500.00, 'Gold', "Curly's Creek, NM", 'example.com/nugget.jpg'),
-#     Treasure("Fool's Gold", 0, 'Pyrite', "Fool's Fails, CO", 'cdn.shopify.com/s/files/1/0081/4402/products/sticker_grande.jpg?v=1479972797'),
-#     Treasure('Coffee Can', 20.00, 'Tin', 'Acme, CA', 'example.com/coffee-can.jpg')
-# ]
-
 def profile(request, username):
     user = User.objects.get(username=username)
     treasures = Treasure.objects.filter(user=user)
     return render(request, 'profile.html', {'username': username, 'treasures':treasures})
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -72,3 +54,15 @@ def login_view(request):
     else:
         form = LoginForm()
         return render(request, 'login.html', {'form': form})
+
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/')
+
+# @login_required
+# def logout(request):
+#     auth.logout(request)
+#     response = HttpResponseRedirect('/')
+#     return response
